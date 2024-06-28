62e532e816f58096449f1454c0729851bd38f784
vulenamjp <vulenam.work@gmail.com>
2024-06-29 07:27:06 +0900

    Update CSS, add MUI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>Header here</header>
+        <div id="mainContent">{children}</div>
+        <footer>Footer here</footer>
+      </body>
     </html>
   );
 }
