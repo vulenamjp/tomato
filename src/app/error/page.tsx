af7ba8d1220b223976791bc1406891968409495c
vulenamjp <vulenam.work@gmail.com>
2024-07-02 22:09:20 +0900

    Update error page

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -1,21 +1,7 @@
-"use client";
-import { useEffect } from "react";
-
-export default function Error({
-  error,
-  reset,
-}: {
-  error: Error & { digest?: string };
-  reset: () => void;
-}) {
-  useEffect(() => {
-    console.error(error);
-  }, [error]);
-
+export default function Error() {
   return (
     <div>
       <h2>Something went wrong!</h2>
-      <button onClick={reset}>Try again</button>
     </div>
   );
 }
